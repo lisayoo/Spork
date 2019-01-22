4d9486688fc37012e3301c217d944881d11fe5f1
Kathryn Wicks <mitadm@77DL5H2.mit.edu>
2019-01-22 17:50:52 -0500

    adds add recpie button to nav

--- public/js/viewrecipe.js
+++ public/js/viewrecipe.js
@@ -17,24 +17,6 @@ function popRecipe(recipe) {
   edit.href ="/edit"+"?"+recipe;
 }
 
-
-// function submitRecipeEdit() {
-//   // TO BE IMPLEMENTED:
-//   // submit the story to our newly implemented database
-//   const parent = window.location.search.substring(1);
-//   const rtitle = document.getElementById('recipetitle').value;
-//   const rdesc = document.getElementById('recipedescription').value;
-//   const ringr = document.getElementById('ingredients').value;
-//   const rsteps = document.getElementById('steps').value;
-
-//   const data = {
-//     "p" : parent,
-//     "rt": rtitle,
-//     "rd": rdesc,
-//     "ri": ringr,
-//     "rs": rsteps  };
-//   post('/api/editrecipe', data);
-// }
 const recipeId = window.location.search.substring(1);
 popRecipe(recipeId);
 // let submit = document.getElementById("recipesubmit")
