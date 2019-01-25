f6492b802dd063f4083e80459bb714fdaf35a52c
Lisa <lisayoo@dhcp-18-30-93-174.dyn.mit.edu>
2019-01-25 00:46:28 -0500

    link 2 feed

---
+++ public/js/search.js
@@ -0,0 +1,46 @@
+function storyDOMObject(storyJSON) {
+  const card = document.createElement('div');
+  card.setAttribute('id', storyJSON._id);
+  card.className = 'story';
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  card.appendChild(cardBody);
+
+  const contentSpan = document.createElement('a');
+  contentSpan.className = 'story-content card-title';
+  contentSpan.innerHTML = storyJSON.name;
+  contentSpan.setAttribute('href', '/view?' + storyJSON._id);
+  cardBody.appendChild(contentSpan);
+
+  const creatorSpan = document.createElement('a');
+  creatorSpan.className = 'story-creator card-title';
+  creatorSpan.innerHTML = storyJSON.author;
+  creatorSpan.setAttribute('href', '/u/profile?' + storyJSON.author);
+  console.log('this is ok');
+  cardBody.appendChild(creatorSpan);
+
+  // const cardFooter = document.createElement('div');
+  // cardFooter.className = 'card-footer';
+  // card.appendChild(cardFooter);
+
+  return card;
+}
+
+
+function renderStories() {
+  console.log('calling renderStories');
+  const storiesDiv = document.getElementById('stories');
+  console.log('made storiesDiv');
+  get('/api/feed', {}, function(storiesArr) {
+    console.log('successful get request');
+    for (let i = 0; i < storiesArr.length; i++) {
+      const currentStory = storiesArr[i];
+      storiesDiv.prepend(storyDOMObject(currentStory));
+    }
+  });
+}
+
+renderStories();
+
+
